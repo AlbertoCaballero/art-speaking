d7936371478d6a495c6c5c77e6c6fcdb3bb200f2
AlbertoCaballero <albertocaballero80@outlook.com>
2020-06-26 21:10:19 -0500

    Guard Service for authenticated users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { AboutComponent } from './about/about.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { PieceComponent } from './piece/piece.component';
 import { ContactComponent } from './contact/contact.component';
+import { AuthGuard } from './_services/auth.guard';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'gallery', component: GalleryComponent },
-    { path: 'piece', component: PieceComponent },
+    { path: 'piece', component: PieceComponent, canActivate: [AuthGuard] },
     { path: 'contact', component: ContactComponent }
 ];
 
